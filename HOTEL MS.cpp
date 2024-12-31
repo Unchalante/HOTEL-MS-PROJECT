c4c12aa5e9378d3de679ff1bb3761272f1fbc0c4
unchalante <jonathanchilongani@gmail.com>
2024-12-31 19:17:10 +0300

    Variable declaration,function and object call

---
+++ HOTEL MS.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int quant;
+    int choice;
+    //Quantity
+    int Qrooms=0, Qpasta=0, Qburger=0, Qnoodles=0, Qshake=0, Qchicken=0;
+    //food items sold
+    int Srooms=0, Spasta=0, Sburger=0, Snoodles=0, Sshake=0, Schicken=0;
+    //Total price of items
+    int Total_rooms=0, Total_pasta=0, Total_burger=0, Total_noodles=0, Total_shake=0, Total_chicken=0;
+
+
+    cout<<"\n\t Quantity of items we have";
+    cout<<"\n Rooms available:";
+    cin>>Qrooms;
+    cout<<"\n Quantity of pasta :";
+    cin>>Qpasta;
+}
